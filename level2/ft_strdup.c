f906559739feeb003eac17dc284854131eba67fc
Omar Hamadou <hamadouomar34@gmail.com>
2023-08-25 13:11:07 +0100

    snakecase

--- level2/ft_strdup.c
+++ level2/ft_strdup.c
@@ -1,20 +1,46 @@
-char *ft_strdup(char *src)
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int ft_strlen(char *str)
 {
     int i = 0;
-    int length = 0;
-    char *strcpy;
     
-    while (src[length])
-        length++;
-    strcpy = malloc(sizeof(*strcpy) * (length + 1));
-    if (strcpy != NULL)
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char *ft_strcpy(char *dst, char *src)
+{
+    while (*src)
     {
-        while (src[i])
-        {
-            strcpy[i] = src[i];
-            i++;
-        }
-        strcpy[i] = '\0';
+        *dst = *src;
+        dst++;
+        src++;
     }
-    return (strcpy);
+    *dst = '\0';
+    return (dst);
+}
+
+char    *ft_strdup(char *src)
+{
+    int len = 0;
+    char *dup;
+    
+    len = ft_strlen(src);
+    dup = (char *)malloc(sizeof(char) * len + 1);
+    if (!dup)
+        return (NULL);
+    ft_strcpy(dup, src);
+    return(dup);
+}
+
+int main()
+{
+    char source[] = "TestHere";
+    char* target = ft_strdup(source);
+
+    printf("%s\n", target);
+    return 0;
 }
